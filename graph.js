71bfaf899e28e65820b0f57cb304beff05add3a0
jjchen <junjunc@princeton.edu>
2012-08-04 17:46:38 -0700

    graph functions

--- graph.js
+++ graph.js
@@ -13,31 +13,54 @@ var shadeUnderneath = false;
 var color = new jsColor("red");
 var radius = 3;
 
+// variables for lines
+var lineWidth = 2;
 
 function drawGraph(pixelPoints) {
 	plotPoint(0, points[0]);
 	var pointsLen = pixelPoints.length();
 	for (int i = 1; i < pixelPointsLen; i++) {
-		drawLine(points[0], pixelPoints[i]);
-		plotPoint(i, pixelPoints[i]);
+		drawLine(pixelPoints[i - 1], pixelPoints[i], i);
+		plotPoint(i, pixelPoints[i], i);
 		if (shadeUnderneath) {
-			drawFill(points[0], pixelPoints[i]);
+			drawFill(pixelPoints[i - 1], pixelPoints[i], i);
 		}
+	}
+	if (shadeUnderneath) {
+		drawFill(pixelPoints);
 	}
 }
 
 // literally drawing a tiny circle
 function plotPoint(binIndex, singlePoint) {
+	context.fillStyle = color;
 	context.beginPath();
-	context.arc(binIndex, singlePoint)
-	
-	
+	context.arc(binIndex * binGap, singlePoint, radius, 0, 2*Math.PI, true)
+	context.closePath();
+	context.fill();
 }
 
-function drawLine(pointOne, pointTwo) {
+function drawLine(pointOne, pointTwo, binIndex) {
+	context.beginPath();
+	context.moveTo(binIndex * binGap , pointOne);
+	context.lineTo(binIndex * binGap , pointTwo);
+	context.lineWidth = lineWidth;
+	context.strokeStyle = color;
+	context.stroke();
 }
 
-function drawFill(pointOne, pointTwo) {
+function drawFill(points) {
+	context.beginPath();
+	context.moveTo(0, points[0]);
+	var pointsLen = pixelPoints.length();
+	for (int i = 1; i < pointsLen; i++) {
+		context.lineTo(i * binGap, points[i]); 
+	}
+	context.closePath();
+	context.lineWidth = 0;
+	context.fillStyle = fillColor;
+	context.fill();
+	context.stroke();
 }
 
 function setColor(color) {

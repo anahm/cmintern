208dbbb128d8e9b55fc938869a7a63a1d919360e
jjchen <junjunc@princeton.edu>
2012-08-04 18:20:47 -0700

    controller

--- createDataPoints.js
+++ createDataPoints.js
@@ -1,7 +1,15 @@
 function controller() {
 	//get list from peter
 	//get numberofBins, start and end time
-	var points = listIterator(list, numberOfBins, startTime, endTime);
+	var numberofBins = 0;
+	var startTime = 0;
+	var endTime = 100;
+//	var points = listIterator(list, numberOfBins, startTime, endTime);
+//	var points = new Array();
+	points.push(3);
+	points.push(2);
+	points.push(10);
+	points.push(7);
 	drawGraph(points);	
 }
 

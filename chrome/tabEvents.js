2cb835091ac2fb1d1082df7fd7b92b73e2fd62a7
Peter Zhang <p@peetahzee.com>
2012-08-04 18:36:48 -0700

    Organized files, added new/remove listeners

---
+++ chrome/tabEvents.js
@@ -0,0 +1,8 @@
+chrome.tabs.onCreated.addListener(function(tab) {
+	writeTabEvent(true);
+});
+chrome.tabs.onRemoved.addListener(function(tab) {
+	if(tab.url == null || tab.url == undefined) {
+		writeTabEvent(false);
+	}
+});
---
+++ chrome/utils.js
@@ -0,0 +1,50 @@
+/*
+ * Helper methods
+ */
+function getCurrentTabsNum() {
+	chrome.tabs.query({} /* get ALL the tabs! */, function(tabs) {
+		document.getElementById("currentTabsNum").innerHTML = tabs.length;
+	});
+}
+function getEventsNum() {
+	getTabEvents(function(events) {
+		document.getElementById("eventsNum").innerHTML = events.length;
+	});
+}
+
+function writeTabEvent(created) {
+	getTabEventsToWrite({time: getCurrentTime(), created: created}, function(events) {
+		chrome.storage.local.set({
+			events: events
+		}, function() {
+		});
+	});
+}
+
+function getTabEventsToWrite(eventToWrite, callback) {
+	getTabEvents(function(events) {
+		if(events == null || events == undefined) {
+			console.log("Hello");
+			events = [eventToWrite];
+			callback(events);
+		} else {
+			console.log("Bye");
+			events[events.length] = eventToWrite;
+			console.log(events);
+			callback(events);
+		}
+	});
+}
+
+function getTabEvents(callback) {
+	chrome.storage.local.get("events", function(items) {
+		console.log(items.events);
+		callback(items.events);
+	});
+}
+
+function getCurrentTime() {
+	// returns unix timestamp in seconds.
+	var d = new Date();
+	return parseInt(d.getTime() / 1000);
+}

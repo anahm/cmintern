e7c136578364fad603b63bb26c8ec8a6ed80ee05
Ali <alisnahm@gmail.com>
2012-08-04 17:03:31 -0700

    graph javascript drawing

---
+++ graph.js
@@ -0,0 +1,31 @@
+/*
+ * graph.js
+ *
+ * actually draws the graph using <canvas> for html5
+ */
+
+// creating jsGraphics object
+var gr = new jsgraphics(document.getElementById("canvas");
+var color = new jsColor("red");
+var shadeUnderneath = false;
+
+function drawGraph(points) {
+	plotPoint(points[0]);
+	var pointsLen = points.length();
+	for (int i = 1; i < pointsLen; i++) {
+		drawLine(points[0], points[i], shadeUnderneath);
+		plotPoint(points[i]);
+	}
+}
+
+// literally drawing a tiny circle
+function plotPoint(singlePoint) {
+	
+	
+}
+
+function drawLine(pointOne, pointTwo, shadeUnderneath) {
+}
+
+function setColor(color) {
+}
